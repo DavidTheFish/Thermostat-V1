d41a9a3d4afa8969c1c172673b6dcc99e4f81879
DavidTheFish <Davey80Z@gmail.com>
2017-07-17 14:37:56 -0700

    Update main.cpp

--- learning project/main.cpp
+++ learning project/main.cpp
@@ -2,13 +2,6 @@
 #include "Thermos.h"
 using namespace std;
 
-echo "# probable-couscous" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/DavidTheFish/probable-couscous.git
-git push -u origin master
-
 
 int main()
 {
