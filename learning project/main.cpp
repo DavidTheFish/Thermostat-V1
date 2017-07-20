3435354c68be0a2b983248c8f2a55ce538b7fc77
DavidTheFish <Davey80Z@gmail.com>
2017-07-19 17:08:28 -0700

    Changed living_room
    I have changed living_room from an instance of a class to a pointer pointing to an instance of a class. I have also add a "delete living_room;" on line 16.

--- learning project/main.cpp
+++ learning project/main.cpp
@@ -2,16 +2,16 @@
 #include "Thermos.h"
 using namespace std;
 
-
 int main()
 {
-Thermos living_room;
-living_room.Current_temp = 78;
-living_room.Des_temp = 400;
-living_room.Function = heat;
+Thermos *living_room = new Thermos;
+living_room->Current_temp = 78;
+living_room->Des_temp = 400;
+living_room->Function = heat;
 
-living_room.cool(living_room.Current_temp, living_room.Des_temp);
-living_room.heat(living_room.Current_temp, living_room.Des_temp);
-living_room.stay_const(living_room.Current_temp, living_room.Des_temp);
-cout<<endl<<living_room.Current_temp<<endl<< living_room.Des_temp;
+living_room->cool(living_room->Current_temp, living_room->Des_temp);
+living_room->heat(living_room->Current_temp, living_room->Des_temp);
+living_room->stay_const(living_room->Current_temp, living_room->Des_temp);
+cout<<endl<<living_room->Current_temp<<endl<< living_room->Des_temp;
+delete living_room;
 }
