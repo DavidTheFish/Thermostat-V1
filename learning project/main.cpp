ae29ae663c58da985216938a398f14290b8bb965
DavidTheFish <Davey80Z@gmail.com>
2017-07-21 15:31:24 -0700

    comments
    I have added comments

--- learning project/main.cpp
+++ learning project/main.cpp
@@ -4,14 +4,18 @@ using namespace std;
 
 int main()
 {
+    //creating a new pointer that points to an instance of class Thermos
 Thermos *living_room = new Thermos;
+//setting some variables from class Thermos
 living_room->Current_temp = 78;
 living_room->Des_temp = 400;
 living_room->Function = heat;
-
+//calling functions from class Thermos
 living_room->cool(living_room->Current_temp, living_room->Des_temp);
 living_room->heat(living_room->Current_temp, living_room->Des_temp);
 living_room->stay_const(living_room->Current_temp, living_room->Des_temp);
+//writing results of functions to console
 cout<<endl<<living_room->Current_temp<<endl<< living_room->Des_temp;
+//clearing pointer
 delete living_room;
 }
