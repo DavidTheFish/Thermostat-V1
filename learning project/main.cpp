fa47218e5d97156bf7023367308e236948ff29af
DavidTheFish <Davey80Z@gmail.com>
2017-07-23 11:43:16 -0700

    Controller function added
    The controller function has been added and I think I can finally release the first alpha!

--- learning project/main.cpp
+++ learning project/main.cpp
@@ -8,8 +8,9 @@ int main()
 Thermos *living_room = new Thermos;
 //setting some variables from class Thermos
 living_room->Current_temp = 78;
-living_room->Des_temp = 400;
-living_room->Function = heat;
+living_room->Des_temp = 0;
+living_room->Function = stay_const;
+living_room->controller(living_room);
 //calling functions from class Thermos
 living_room->cool(living_room->Current_temp, living_room->Des_temp);
 living_room->heat(living_room->Current_temp, living_room->Des_temp);
