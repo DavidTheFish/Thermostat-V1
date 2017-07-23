77e89d6d44f34c9ad0b9ecd17d0445aa84004dad
DavidTheFish <Davey80Z@gmail.com>
2017-07-23 11:49:13 -0700

    added more comments and added controller function
    I have added more comments to make the code easier to read and I added code for a function called "controller"

--- learning project/Thermos.cpp
+++ learning project/Thermos.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<conio.h>
 #include "Thermos.h"
 using namespace std;
 
@@ -7,14 +8,14 @@ void Thermos::cool(int &X, int const Y){
     while(X > Y && Function==0){
         X--;
         cout<<X<<endl;
-    }
-}
+    }//end of while
+}//end of cool
 void Thermos::heat(int &X, int const Y){
 while(X < Y && Function == 1){
     X++;
     cout<<X<<endl;
-}
-}
+}//end of while
+}//end of heat
 void Thermos::stay_const(int &X, int const Y){
 while(X > Y && Function == 2){
     X--;
@@ -23,8 +24,23 @@ while(X > Y && Function == 2){
 while(X < Y && Function == 2){
     X++;
     cout<<X<<endl;
-}
-}
+}//end of while
+}//end of stay_const
+void Thermos::controller(Thermos *V){
+char ch = getch();
+while (ch != '\r'){
+    if(ch == 'w'){
+        this->Des_temp++;
+        cout<<this->Des_temp<<endl;
+    }//end of if
+    else if (ch == 's'){
+        this->Des_temp--;
+        cout<<this->Des_temp<<endl;
+    }//end of else if
+    ch = getch();
+}//end of while
+}//end of controller
+
 Thermos :: ~Thermos(){
-cout<<endl<<"class has been deleted";
-}
+cout<<endl<<"all done";
+}//Thermos end
